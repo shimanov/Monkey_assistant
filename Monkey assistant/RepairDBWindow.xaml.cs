1101ca08d3b3908c2e1d6d31a1316989e52f2688
Dima <d.a.shimanov@gmail.com>
2017-08-28 16:44:31 +0700

    Реализация восстановления БД
    1.  Перевод БД в однопользовательский режим 
    2. Восстановление БД
    3. Перевод БД в многопользовательский режим 
    4. Перевод БД в онлайн режим

--- Monkey assistant/RepairDBWindow.xaml.cs
+++ Monkey assistant/RepairDBWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -29,31 +30,158 @@ namespace Monkey_assistant
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            GetpcName getpcName = new GetpcName();
-            SqlConnection connection = new SqlConnection("Server = localhost; "
-                                                         + "Initial Catalog = " + getpcName.NamePc() + ";"
-                                                         + "Integrated Security = SSPI");
+            DialogResult result = System.Windows.Forms.MessageBox.Show("ВНИМАНИЕ!",
+                                                    "НЕОБХОДИМО ЗАВЕРШИТЬ РАБОТУ НА ВСЕХ ОКНАХ, ВКЛЮЧАЯ ДОСТАВКУ!",
+                                                    MessageBoxButtons.YesNo);
 
-            SqlCommand command = new SqlCommand
+            //Переводим в однопользовательский режим.
+            if (result == System.Windows.Forms.DialogResult.Yes)
             {
-                CommandText = "DBCC CHECKDB (" + "'DB" + getpcName.NamePc() + "'" + ",REPAIR_ALLOW_DATA_LOSS);",
-                CommandType = CommandType.Text,
-                Connection = connection
-            };
-            try
-            {
-                connection.Open();
+                GetpcName getpcName = new GetpcName();
+                SqlConnection connection = new SqlConnection("Server = localhost; "
+                                                             + "Initial Catalog = " + getpcName.NamePc() + ";"
+                                                             + "Integrated Security = SSPI");
 
-                //Logger.Log.Info("Успешно подключились к БД " + NamePc() + "для выполнения скрипта ReplicaExport");
-                command.CommandTimeout = 240;
-                command.ExecuteNonQuery();
-                connection.Close();
-                //Logger.Log.Info("Успешно выполнил скрипт ReplicaExport на БД " + NamePc() + "соединение закрыл");
+                #region Переводим БД в однопользовательский режим
+                SqlCommand command = new SqlCommand
+                {
+                    CommandText = "ALTER DATABASE " + "DB" + getpcName.NamePc() + "SET SINGLE_USER",
+                    CommandType = CommandType.Text,
+                    Connection = connection
+                };
+                try
+                {
+                    connection.Open();
+
+                    //Logger.Log.Info("Успешно подключились к БД " + NamePc() + "для выполнения скрипта ReplicaExport");
+                    command.CommandTimeout = 240;
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    //Logger.Log.Info("Успешно выполнил скрипт ReplicaExport на БД " + NamePc() + "соединение закрыл");
+                }
+                catch (Exception exception)
+                {
+                    //Logger.Log.Error(exception.Message);
+                }
+
+                #endregion
+
+                #region Восстанавливаем БД
+
+                SqlCommand command1 = new SqlCommand
+                {
+                    CommandText = "DBCC CHECKDB (" + "'DB" + getpcName.NamePc() + "'" + ",REPAIR_ALLOW_DATA_LOSS);",
+                    CommandType = CommandType.Text,
+                    Connection = connection
+                };
+                try
+                {
+                    connection.Open();
+
+                    //Logger.Log.Info("Успешно подключились к БД " + NamePc() + "для выполнения скрипта ReplicaExport");
+                    command1.CommandTimeout = 240;
+                    command1.ExecuteNonQuery();
+                    connection.Close();
+                    //Logger.Log.Info("Успешно выполнил скрипт ReplicaExport на БД " + NamePc() + "соединение закрыл");
+                }
+                catch (Exception exception)
+                {
+                    //Logger.Log.Error(exception.Message);
+                }
+
+                #endregion
+
+                //TODO: Реализовать вывод процента выполнения
+                #region Выводим процент выполнения
+
+                SqlCommand command3 = new SqlCommand
+                {
+                    CommandText = "SELECT [command],[start_time],[percent_complete] FROM sys.dm_exec_requests WHERE [command] LIKE '%DBCC%'",
+                    CommandType = CommandType.Text,
+                    Connection = connection
+                };
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command3.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var percent = reader.GetDouble(2);
+                        percentLabel.Content = percent;
+                    }
+
+                    //Logger.Log.Info("Успешно подключились к БД " + NamePc() + "для выполнения скрипта ReplicaExport");
+                    command3.CommandTimeout = 240;
+                    command3.ExecuteNonQuery();
+                    connection.Close();
+                    //Logger.Log.Info("Успешно выполнил скрипт ReplicaExport на БД " + NamePc() + "соединение закрыл");
+                }
+                catch (Exception exception)
+                {
+                    //Logger.Log.Error(exception.Message);
+                }
+
+                #endregion
+
+                #region Переводим в многопользовательский режим
+
+                SqlCommand command2 = new SqlCommand
+                {
+                    CommandText = "ALTER DATABASE " + "DB" + getpcName.NamePc() + "SET MULTI_USER",
+                    CommandType = CommandType.Text,
+                    Connection = connection
+                };
+                try
+                {
+                    connection.Open();
+
+                    //Logger.Log.Info("Успешно подключились к БД " + NamePc() + "для выполнения скрипта ReplicaExport");
+                    command2.CommandTimeout = 240;
+                    command2.ExecuteNonQuery();
+                    connection.Close();
+                    //Logger.Log.Info("Успешно выполнил скрипт ReplicaExport на БД " + NamePc() + "соединение закрыл");
+                }
+                catch (Exception exception)
+                {
+                    //Logger.Log.Error(exception.Message);
+                }
+
+                #endregion
+
+                #region Переводим в онлайн режим
+
+                SqlCommand command4 = new SqlCommand
+                {
+                    CommandText = "ALTER DATABASE " + "DB" + getpcName.NamePc() + "SET ONLINE",
+                    CommandType = CommandType.Text,
+                    Connection = connection
+                };
+                try
+                {
+                    connection.Open();
+
+                    //Logger.Log.Info("Успешно подключились к БД " + NamePc() + "для выполнения скрипта ReplicaExport");
+                    command4.CommandTimeout = 240;
+                    command4.ExecuteNonQuery();
+                    connection.Close();
+                    //Logger.Log.Info("Успешно выполнил скрипт ReplicaExport на БД " + NamePc() + "соединение закрыл");
+                }
+                catch (Exception exception)
+                {
+                    //Logger.Log.Error(exception.Message);
+                }
+
+                #endregion
             }
-            catch (Exception exception)
+
+            //Закрываем окно, если нажали - нет
+            else
             {
-                //Logger.Log.Error(exception.Message);
+                RepairDBWindow repairDBWindow = new RepairDBWindow();
+                repairDBWindow.Close();
             }
+
+            
         }
     }
 }
